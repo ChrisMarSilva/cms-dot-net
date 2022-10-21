c7d1bc8051ecd2e393bc24086656b21ff7af8caa
ChrisMarSilva <chris.mar.silva@gmail.com>
2022-10-21 18:34:22 +0100

    add tests with api and redis

---
+++ CMS DotNet Teste API And WEB/CMS DotNet Teste WebAPI With Cache Redis/Controllers/ProcController.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StackExchange.Redis;
+using System.Net;
+// using Microsoft.Extensions.Caching.Memory;
+
+namespace WebApplication1.Controllers
+{
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProcController : ControllerBase
+    {
+        private readonly ILogger<ProcController> _logger;
+        private readonly IConnectionMultiplexer _connectionMultiplexer;
+        // private readonly IMemoryCache _memoryCache;
+        // private static MemoryCacheEntryOptions _cacheEntryOptions;
+
+        public ProcController(ILogger<ProcController> logger, IConnectionMultiplexer connectionMultiplexer /* , IMemoryCache memoryCache*/)
+        {
+            _logger = logger;
+            _connectionMultiplexer = connectionMultiplexer;
+            // _memoryCache = memoryCache;
+            // _cacheEntryOptions = GetCacheEntryOptions();
+        }
+
+        [HttpPost("")]
+        public async Task<IActionResult> SetAsync(string key, string value)
+        {
+            try
+            {
+                _logger.LogInformation($"CALL: SetAsync - key={key} - value={value}");
+
+                // _memoryCache.Set(key, value, MemoryCacheEntryOptions(){ AbsoluteExpiration = DateTimeOffset.Now.AddSeconds(40), SlidingExpiration = TimeSpan.FromSeconds(10)});
+                //_memoryCache.Set(key, value, _cacheEntryOptions);
+
+                var db = _connectionMultiplexer.GetDatabase();
+                // await db.StringSetAsync(key, value);
+                await db.StringSetAsync(key, value, flags: CommandFlags.FireAndForget);
+
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"FAILED: SetAsync - key={key} - value={value} - erro={ex.Message}");
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+                //return StatusCode(StatusCodes.Status500InternalServerError, "Internal Server Error. Something went Wrong!");
+            }
+        }
+
+        [HttpGet("")]
+        public async Task<IActionResult> GetAsync(string key)
+        {
+            try
+            {
+                _logger.LogInformation($"CALL: GetAsync - key={key}");
+
+                // if (!_memoryCache.TryGetValue(key, out string value))
+                //     return NotFound();
+
+                var db = _connectionMultiplexer.GetDatabase();
+
+                string value = await db.StringGetAsync(key);
+                _logger.LogInformation($"CALL: GetAsync - key={key} - value={value}");
+
+                if (string.IsNullOrEmpty(value))
+                {
+                    value = "123"; // valor default // ou pegar do Banco de Dados
+                    await db.StringSetAsync(key, value);
+                    // return NotFound();
+                }
+
+                return Ok(value);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"FAILED: GetAsync - key={key} - erro={ex.Message}");
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+                //return StatusCode(StatusCodes.Status500InternalServerError, "Internal Server Error. Something went Wrong!");
+            }
+        }
+
+        // private static MemoryCacheEntryOptions GetCacheEntryOptions()
+        // {
+        //     return new MemoryCacheEntryOptions()
+        //         .SetAbsoluteExpiration(TimeSpan.FromSeconds(100)) // 100 // 40
+        //         .SetSlidingExpiration(TimeSpan.FromSeconds(100)); // 100 // 10
+        // }
+
+    }
+}
+
+
+public interface ICacheAccessor
+{
+    Task SetAsync(string value, string key);
+    Task<string> GetAsync(string key);
+}
