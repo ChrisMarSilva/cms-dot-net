9429958204f2e094bf2800213f2ae06ab0950104
ChrisMarSilva <chris.mar.silva@gmail.com>
2023-02-15 20:41:49 +0000

    add CMS DotNet Curso .NET 6 Arquitetura de Microsserviços

--- CMS DotNet Cursos/CMS DotNet Curso .NET 6 Arquitetura de Microsserviços/GeekShopping/GeekShopping.ProductAPI/Program.cs
+++ CMS DotNet Cursos/CMS DotNet Curso .NET 6 Arquitetura de Microsserviços/GeekShopping/GeekShopping.ProductAPI/Program.cs
@@ -1,23 +1,34 @@
+using GeekShopping.ProductAPI.Model.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+var connection = builder.Configuration["ConnectionStrings:GeekShoppingProductAPI"];
+builder.Services.AddDbContext<MySQLContext>(options => options.UseMySql(connection, new MySqlServerVersion(new Version(5, 6, 0))));
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options => { options.SwaggerDoc("v1", new OpenApiInfo { Title = "GeekShopping.ProductAPI", Version = "v1" }); });
 
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
+
+// dotnet tool update --global dotnet-ef
+// dotnet build 
+
+// dotnet ef migrations add xxxxx
+// dotnet ef database update
+// dotnet ef migrations remove
+
+// DTO = Data Transfer Object
+// VO = Value Object
+
