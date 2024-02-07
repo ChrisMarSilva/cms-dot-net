58b08adbf3595469f5c64b74c67edb88dd545db3
ChrisMarSilva <chris.mar.silva@gmail.com>
2024-02-07 01:43:01 +0000

    Add Benchmarks

---
+++ CMS DotNet Teste Outros/CMS DotNet Teste Benchmark/Benchmarks/Benchmark.cs
@@ -0,0 +1,21 @@
+﻿using TesteBenchmarkDotNet.Data;
+
+namespace TesteBenchmarkDotNet.Benchmarks
+{
+
+    // https://nishanc.medium.com/writing-better-performant-queries-with-linq-on-ef-core-6-0-%EF%B8%8F-85a1a406879
+    public class Benchmark : IDisposable
+    {
+        protected readonly DataContext Context;
+
+        public Benchmark()
+        {
+            Context = new DataContext();
+        }
+
+        public void Dispose()
+        {
+            Context.Dispose();
+        }
+    }
+}
