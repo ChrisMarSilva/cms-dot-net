1824e42e97fdb774761d76e5676dd69a8c67acd6
ChrisMarSilva <chris.mar.silva@gmail.com>
2025-01-04 15:02:50 -0300

    Add Benchmark Numbers

---
+++ CMS DotNet Teste Benchmark/CMS DotNet Teste Benchmark Numbers/BenchmarkNumbers.cs
@@ -0,0 +1,71 @@
+﻿using BenchmarkDotNet.Attributes;
+using System.Collections;
+
+namespace CMS_DotNet_Teste_Benchmark_Numbers;
+
+[MemoryDiagnoser]
+public class BenchmarkNumbers
+{
+    private List<int> numbers;
+    private ArrayList arrayList;
+
+    [GlobalSetup]
+    public void Setup()
+    {
+        numbers = new List<int> { 1, 2, 3, 4, 5, 3, 2, 6, 7, 5, 2, 3, 4, 5, 6, 7, 7, 8, 8, 8 };
+        arrayList = new ArrayList { 1, 2, 3, 4, 5, 3, 2, 6, 7, 5, 2, 3, 4, 5, 6, 7, 7, 8, 8, 8 };
+    }
+
+    // Método para encontrar os duplicados usando Dictionary
+    [Benchmark]
+    public List<int> FindDuplicatesWithDictionary()
+    {
+        var countDictionary = new Dictionary<int, int>();
+        foreach (var number in numbers)
+        {
+            if (countDictionary.ContainsKey(number))
+                countDictionary[number]++;
+            else
+                countDictionary[number] = 1;
+        }
+
+        return countDictionary.Where(kvp => kvp.Value > 1).Select(kvp => kvp.Key).ToList();
+    }
+
+    // Método para encontrar os duplicados usando LINQ
+    [Benchmark]
+    public List<int> FindDuplicatesWithLinq()
+    {
+        return numbers.GroupBy(n => n)
+                      .Where(g => g.Count() > 1)
+                      .Select(g => g.Key)
+                      .ToList();
+    }
+
+    [Benchmark]
+    public ArrayList FindDuplicates()
+    {
+        //var seen = new HashSet<int>();
+        //var duplicates = new HashSet<int>();
+
+        Hashtable seen = new Hashtable();
+        ArrayList duplicates = new ArrayList();
+
+        foreach (var number in arrayList)
+        {
+            if (seen.ContainsKey(number))
+            {
+                if (!duplicates.Contains(number))
+                {
+                    duplicates.Add(number);
+                }
+            }
+            else
+            {
+                seen[number] = true;
+            }
+        }
+
+        return duplicates;
+    }
+}
---
+++ CMS DotNet Teste Benchmark/CMS DotNet Teste Benchmark Numbers/Program.cs
@@ -0,0 +1,17 @@
+﻿using BenchmarkDotNet.Running;
+using CMS_DotNet_Teste_Benchmark_Numbers;
+
+Console.WriteLine("INI");
+try
+{
+    BenchmarkRunner.Run<BenchmarkNumbers>();
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"ERRO: {ex.Message}");
+}
+finally
+{
+    Console.WriteLine("FIM");
+    Console.ReadKey();
+}
